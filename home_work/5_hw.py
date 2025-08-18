8903e74b0aef11f6b059ba5758a402dc5ab1bb44
Kseniya Troyanova <ksetru@mail.ru>
2025-08-18 22:38:58 +0300

    Update 5_hw.py

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -6,23 +6,32 @@
 #  - кнопку submit
 # c. Создайте условие, если элементы найдены - вывести в консоль текст “Элементы найдены”
 
+# Переход на страницу https://www.saucedemo.com/
+# При отработке открывается сайт
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-
 driver = webdriver.Chrome()
+driver.get ('https://www.saucedemo.com/')
 
-try:
-    driver.get("https://www.saucedemo.com/")
+username = driver.find_element(By.CSS_SELECTOR, '#user-name')
+if username is None:
+    print('Текстовое поле Username не найдено')
+else:
+    print('Текстовое поле Username найдено')
 
-    # Селекторы для этого сайта
-    username_field = driver.find_element(By.ID, "user-name")
-    password_field = driver.find_element(By.ID, "password")
-    login_button = driver.find_element(By.ID, "login-button")
+password = driver.find_element(By.CSS_SELECTOR, '#password')
+if password is None:
+    print('Текстовое поле Password не найдено')
+else:
+    print('Текстовое поле Password найдено')
 
-    print("Все элементы найдены")
+submit_button = driver.find_element(By.CSS_SELECTOR, '#login-button')
+if submit_button is None:
+    print('Кнопка Submit не найдена')
+else:
+    print('Кнопка Submit найдена')
 
-except NoSuchElementException:
-    print("Ошибка: какой-то элемент не найден")
-finally:
-    driver.quit()
+# Результаты теста: Открылся браузер, далее, прошли проверки наличия текстовых полей и кнопки.
+# Текстовое поле Username найдено
+# Текстовое поле Password найдено
+# Кнопка Submit найдена
