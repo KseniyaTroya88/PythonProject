8a619870a5e7b5ffd39d21de0809f2eb8d4c59a3
Kseniya Troyanova <ksetru@mail.ru>
2025-08-20 23:01:22 +0300

    Update 5_hw.py
    2nd improvement

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -6,32 +6,42 @@
 #  - кнопку submit
 # c. Создайте условие, если элементы найдены - вывести в консоль текст “Элементы найдены”
 
-# Переход на страницу https://www.saucedemo.com/
-# При отработке открывается сайт
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
-driver.get ('https://www.saucedemo.com/')
 
-username = driver.find_element(By.CSS_SELECTOR, '#user-name')
-if username is None:
-    print('Текстовое поле Username не найдено')
-else:
-    print('Текстовое поле Username найдено')
 
-password = driver.find_element(By.CSS_SELECTOR, '#password')
-if password is None:
-    print('Текстовое поле Password не найдено')
-else:
-    print('Текстовое поле Password найдено')
+def check_saucedemo_elements():
+    driver = webdriver.Chrome()
+    driver.get('https://www.saucedemo.com/')
 
-submit_button = driver.find_element(By.CSS_SELECTOR, '#login-button')
-if submit_button is None:
-    print('Кнопка Submit не найдена')
-else:
-    print('Кнопка Submit найдена')
+    # селекторы:
+    username_list = driver.find_elements(By.CSS_SELECTOR, '#user-name')
+    password_list = driver.find_elements(By.CSS_SELECTOR, '#password')
+    submit_list = driver.find_elements(By.CSS_SELECTOR, '#login-button')
+
+    if username_list:
+        print('Текстовое поле Username найдено')
+    else:
+        print('Текстовое поле Username не найдено')
+
+    if password_list:
+        print('Текстовое поле Password найдено')
+    else:
+        print('Текстовое поле Password не найдено')
+
+    if submit_list:
+        print('Кнопка Submit найдена')
+    else:
+        print('Кнопка Submit не найдена')
+
+    if username_list and password_list and submit_list:
+        print("Элементы найдены")
+
+
+check_saucedemo_elements()
 
 # Результаты теста: Открылся браузер, далее, прошли проверки наличия текстовых полей и кнопки.
 # Текстовое поле Username найдено
 # Текстовое поле Password найдено
 # Кнопка Submit найдена
+# Элементы найдены
